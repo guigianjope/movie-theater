da363519ed0a2d679e5a951adbc35f605eaa9439
Guilherme Gianjope <guigianjope@gmail.com>
2021-04-28 20:51:52 -0700

    responsive

--- js/main.js
+++ js/main.js
@@ -1,25 +1,30 @@
 
 //carousel
+
 const arrows = document.querySelectorAll(".arrow");
 const movieLists = document.querySelectorAll(".movie-list");
 
-arrows.forEach((arrow, i)=> {
-    const itemNumber = movieLists[i].querySelectorAll('img').length;
+arrows.forEach((arrow, i) => {
+    const itemNumber = movieLists[i].querySelectorAll("img").length;
     let clickCounter = 0;
-    arrow.addEventListener("click", ()=> {
-        clickCounter++;
-        if(itemNumber - (4+clickCounter) > 0){
-            movieLists[i].style.transform = `translateX(
-                ${movieLists[i].computedStyleMap().get("transform")[0].x.value-300}px
-            )`;
-        } else {
-            movieLists[i].style.transform = 'translateX(0)';
-            clickCounter = 0;
-        }        
+    arrow.addEventListener("click", () => {
+      const ratio = Math.floor(window.innerWidth / 320);
+      clickCounter++;
+      if (itemNumber - (5 + clickCounter) + (5 - ratio) >= 0) {
+        movieLists[i].style.transform = `translateX(${
+          movieLists[i].computedStyleMap().get("transform")[0].x.value - 300
+        }px)`;
+      } else {
+        movieLists[i].style.transform = "translateX(0)";
+        clickCounter = 0;
+      }
     });
-});
+  
+    console.log(Math.floor(window.innerWidth / 270));
+  });
 
-//dark mode change
+//toggle: dark/light mode
+
 const ball = document.querySelector(".toggle-ball");
 const items = document.querySelectorAll(".container,.movie-list-item-title,.navbar,.sidebar,.toggle,.toggle-ball,.toggle-icon");
 
